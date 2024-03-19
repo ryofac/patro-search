d31b3024d3ae63232aafe6b87338b93860560c83
LuisF Patrocinio <patrocinioluisf@gmail.com>
2024-03-19 13:25:17 -0300

    feat: documentação da classe View

--- build/app/views/view.js
+++ build/app/views/view.js
@@ -1,49 +1,88 @@
 import { question } from "readline-sync";
 import chalk from "chalk";
+/**
+ * Classe abstrata que representa uma View.
+ */
 export class View {
+    /**
+     * Construtor da classe.
+     * @param viewHandler - Referência do gerenciador de views.
+     */
     constructor(viewHandler) {
         this.viewHandler = viewHandler;
     }
+    /**
+     * Muda a view atual para a view desejada.
+     * @param desiredView - A view desejada.
+     */
     changeView(desiredView) {
         this.viewHandler.goToView(desiredView);
     }
+    /**
+     * Retorna para a view anterior, removendo o topo da pilha.
+     */
     returnView() {
         this.viewHandler.goBackView();
     }
+    /**
+     * Retorna a largura do terminal.
+     * @returns A largura do terminal.
+     */
     getWidth() {
         return process.stdout.columns;
     }
+    /**
+     * Exibe um texto na tela, podendo opcionalmente fazer uso de um dicionário de TextOptions.
+     * @param text Texto a ser exibido.
+     * @param options TextOptions a serem aplicadas ao texto.
+     */
     showText(text, options = {}) {
-        // Atribuindo os valores dicionario de opções, desenpacotando, se existirem;
+        // Cria variáveis com as propriedades das opções, caso existam.
         const { color = chalk.whiteBright, backgroundColor = chalk.blackBright, centered = false } = options;
+        // Modificando o texto com as opções desejadas.
         var printText = backgroundColor(color(text));
+        // Se o texto deve ser centralizado, calcula o padding necessário.
         if (centered) {
             const width = this.getWidth();
             const padding = Math.max(0, Math.floor((width - text.length) / 2));
             printText = ' '.repeat(padding) + printText + ' '.repeat(padding);
         }
+        // Exibindo o texto.
         console.log(printText);
     }
+    /**
+     * Modifica um texto com as opções desejadas.
+     * @param text Texto a ser modificado.
+     * @param options TextOptions a serem aplicadas ao texto.
+     * @returns O texto modificado.
+     */
     modifyText(text, options = {}) {
+        // Cria variáveis com as propriedades das opções, caso existam.
         const { color = chalk.whiteBright, backgroundColor = chalk.blackBright, centered = false } = options;
-        var modifiyed = backgroundColor(color(text));
+        // Modificando o texto com as opções desejadas.
+        var modified = backgroundColor(color(text));
+        // Se o texto deve ser centralizado, calcula o padding necessário.
         if (centered) {
             const width = this.getWidth();
             const padding = Math.max(0, Math.floor((width - text.length) / 2));
-            modifiyed = ' '.repeat(padding) + modifiyed + ' '.repeat(padding);
+            modified = ' '.repeat(padding) + modified + ' '.repeat(padding);
         }
-        return modifiyed;
+        return modified;
     }
+    /** Limpa o Terminal */
     clearTerminal() {
         console.clear();
-        console.clear();
     }
+    /** Move o Cursor para a linha desejada. */
     moveCursorToLine(lineNumber) {
-        process.stdout.write(`\x1b[${lineNumber}H`);
+        // process.stdout.write(`\x1b[${lineNumber}H`);
+        process.stdout.cursorTo(0, lineNumber);
     }
+    /** Move o Cursor para a linha anterior. */
     moveCursorToAnteriorLine() {
-        process.stdout.write('\x1b[F');
+        process.stdout.moveCursor(0, -1);
     }
+    /** Aguarda a ação do usuário para avançar a execução do programa. */
     enterToContinue() {
         this.showText("[ PRESSIONE ENTER PARA CONTINUAR ] ", { centered: true, backgroundColor: chalk.bgWhiteBright, color: chalk.blackBright });
         this.moveCursorToAnteriorLine();
@@ -53,6 +92,7 @@ export class View {
         });
         this.clearTerminal();
     }
+    /** Desenha uma divisória horizontal */
     showLines() {
         console.log('');
         for (let i = 0; i < Math.floor(this.getWidth() / 2); i++) {
@@ -60,6 +100,10 @@ export class View {
         }
         console.log('');
     }
+    /**
+     * Recebe um input do usuário.
+     * @returns O input do usuário, em string.
+     */
     getInput() {
         return question("> ").trimStart().trimEnd();
     }
--- src/app/views/view.ts
+++ src/app/views/view.ts
@@ -3,77 +3,122 @@ import { ViewHandler } from "./viewHandler.js";
 
 import chalk, { Chalk, ColorName } from "chalk";
 
+/**
+ * Representa as opções de texto que podem ser passadas para a função showText.
+ */
 interface TextOptions {
+    /** Cor do Texto */
     color?: CallableFunction;
+
+    /** A cor do fundo do texto.*/
     backgroundColor?: CallableFunction;
+
+    /** Se o texto deve ser centralizado na tela. */
     centered?: boolean;
 }
 
+/**
+ * Classe abstrata que representa uma View.
+ */
 export abstract class View {
 
+    /** Referência do gerenciador de views. */
     viewHandler : ViewHandler;
 
+    /**
+     * Construtor da classe.
+     * @param viewHandler - Referência do gerenciador de views.
+     */
     constructor(viewHandler: ViewHandler) {
         this.viewHandler = viewHandler;
     }
 
+    /**
+     * Muda a view atual para a view desejada.
+     * @param desiredView - A view desejada.
+     */
     changeView(desiredView: View): void {
         this.viewHandler.goToView(desiredView);
     }
 
+    /**
+     * Retorna para a view anterior, removendo o topo da pilha.
+     */
     returnView(): void {
         this.viewHandler.goBackView();
     }
 
+    /**
+     * Retorna a largura do terminal.
+     * @returns A largura do terminal.
+     */
     getWidth() : number {
         return process.stdout.columns;
     }
 
+    /**
+     * Exibe um texto na tela, podendo opcionalmente fazer uso de um dicionário de TextOptions.
+     * @param text Texto a ser exibido.
+     * @param options TextOptions a serem aplicadas ao texto.
+     */
     showText(text: string, options: TextOptions = {}) : void {
-        // Atribuindo os valores dicionario de opções, desenpacotando, se existirem;
+        // Cria variáveis com as propriedades das opções, caso existam.
         const {color = chalk.whiteBright, backgroundColor = chalk.blackBright, centered = false} = options;
 
+        // Modificando o texto com as opções desejadas.
         var printText = backgroundColor(color(text));
 
-        if(centered){
+        // Se o texto deve ser centralizado, calcula o padding necessário.
+        if (centered) {
             const width = this.getWidth();
             const padding = Math.max(0, Math.floor((width - text.length) / 2));
             printText =  ' '.repeat(padding) + printText + ' '.repeat(padding);
         }
 
+        // Exibindo o texto.
         console.log(printText)
-        
     }
 
+    /**
+     * Modifica um texto com as opções desejadas.
+     * @param text Texto a ser modificado.
+     * @param options TextOptions a serem aplicadas ao texto.
+     * @returns O texto modificado.
+     */
     modifyText(text: string, options: TextOptions = {}): string{
+        // Cria variáveis com as propriedades das opções, caso existam.
         const {color = chalk.whiteBright, backgroundColor = chalk.blackBright, centered = false} = options;
 
-        var modifiyed = backgroundColor(color(text));
+        // Modificando o texto com as opções desejadas.
+        var modified = backgroundColor(color(text));
 
-        if(centered){
+        // Se o texto deve ser centralizado, calcula o padding necessário.
+        if (centered) {
             const width = this.getWidth();
             const padding = Math.max(0, Math.floor((width - text.length) / 2));
-            modifiyed =  ' '.repeat(padding) + modifiyed + ' '.repeat(padding);
+            modified =  ' '.repeat(padding) + modified + ' '.repeat(padding);
         }
 
-        return modifiyed;
-
+        return modified;
     }
 
+    /** Limpa o Terminal */
     clearTerminal(): void {
         console.clear();
-        console.clear();
     }
 
+    /** Move o Cursor para a linha desejada. */
     moveCursorToLine(lineNumber: number): void{
-        process.stdout.write(`\x1b[${lineNumber}H`);
+        // process.stdout.write(`\x1b[${lineNumber}H`);
+        process.stdout.cursorTo(0, lineNumber);
     }
 
+    /** Move o Cursor para a linha anterior. */
     moveCursorToAnteriorLine(): void{
-        process.stdout.write('\x1b[F');
-
+        process.stdout.moveCursor(0, -1);
     }
 
+    /** Aguarda a ação do usuário para avançar a execução do programa. */
     enterToContinue() {
         this.showText("[ PRESSIONE ENTER PARA CONTINUAR ] ", {centered : true, backgroundColor : chalk.bgWhiteBright, color : chalk.blackBright});
         this.moveCursorToAnteriorLine();
@@ -85,6 +130,7 @@ export abstract class View {
         this.clearTerminal();
     }
 
+    /** Desenha uma divisória horizontal */
     showLines() {
         console.log('');
         for(let i = 0; i < Math.floor(this.getWidth() /  2); i++){
@@ -93,12 +139,14 @@ export abstract class View {
         console.log('');
     }
 
+    /**
+     * Recebe um input do usuário.
+     * @returns O input do usuário, em string.
+     */
     getInput(): string {
         return question("> ").trimStart().trimEnd();
     }
 
-
-
-
+    /** Função de execução de cada View, que deverá ser implementada. */
     public abstract execute(): void;
 }
